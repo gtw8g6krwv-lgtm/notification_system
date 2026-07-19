fd2f3ef1d9cd95745c53e24d91e23335b2757f4e
daraborisenko <dashaborisenko2002@mail.ru>
2026-07-19 22:46:48 +1000

    Добавлено взаимодействие с классом UserSession

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from user_session import UserSession
+
+
 def main():
     print("\nЗдравствуйте!")
     username = input("Пожалуйста, введите ваше имя: ")
     print(f"Добро пожаловать, {username} в систему оповещений!")
+
+    user = UserSession(username, True)
+    user.send_notification()
+
 
 if __name__ == "__main__":
     main()
