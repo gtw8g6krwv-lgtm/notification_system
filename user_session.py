f08447ce344fa58cce1b82ad6539fe49fe824646
daraborisenko <dashaborisenko2002@mail.ru>
2026-07-19 22:46:03 +1000

    Добавлен метод отправки уведомления

--- user_session.py
+++ user_session.py
@@ -2,3 +2,9 @@ class UserSession:
     def __init__(self, username: str, has_alert_access: bool):
         self._username = username
         self._has_alert_access = has_alert_access
+
+    def send_notification(self):
+        if self._has_alert_access:
+            print(f"[УВЕДОМЛЕНИЕ] Пользователю {self._username} отправлено уведомление о входе в систему")
+        else:
+            print(f"[УВЕДОМЛЕНИЕ] Пользователь {self._username} не имеет доступа к оповещениям")
